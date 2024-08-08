62fa07db3c88eb99851d63bea59ee019c04848c3
aadarshraj02 <aadarshraazz007@gmail.com>
2024-08-08 19:26:31 +0530

    styled div

--- client/src/components/HeroSection.jsx
+++ client/src/components/HeroSection.jsx
@@ -1,13 +1,13 @@
 const HeroSection = () => {
   return (
-    <div>
-      <form className="sm:w-[60vw] h-[20vh] overflow-clip sm:rounded-3xl mx-auto flex justify-center items-center">
+    <div className="sm:w-[60vw] h-[20vh] overflow-clip sm:rounded-3xl mx-auto flex justify-center items-center">
+      <form className="absolute flex justify-center items-center">
         <input
           type="search"
           id="search"
           name="search"
           placeholder="Search your asset..."
-          className=""
+          className="py-5 px-3 w-[80vw] text-xl sm:text-3xl mx-auto outline-none border-b-2"
         />
       </form>
     </div>
