fb3bc6667fb586020392f44e79d978f1acd41776
aadarshraj02 <aadarshraazz007@gmail.com>
2024-08-08 21:09:51 +0530

    added p and styled it show author name

--- client/src/components/PhotoGallery.jsx
+++ client/src/components/PhotoGallery.jsx
@@ -3,12 +3,17 @@ const PhotoGallery = () => {
     <div className="my-20 bg-white flex flex-col justify-center items-center">
       <h3 className="text-3xl font-semibold my-14">Photos</h3>
       <div>
-        <div>
-          <img
-            className="w-full h-full hover:scale-105 transition-all ease-linear duration-300 transform"
-            src="https://plus.unsplash.com/premium_photo-1722586209835-46ae3736ec12?q=80&w=1535&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-            alt="image"
-          />
+        <div className="rounded-lg bg-white shadow-lg p-2">
+          <div className="w-full h-[200px] overflow-hidden rounded-2xl ">
+            <img
+              className="w-full h-full hover:scale-105 transition-all ease-linear duration-300 transform"
+              src="https://plus.unsplash.com/premium_photo-1722586209835-46ae3736ec12?q=80&w=1535&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+              alt="image"
+            />
+          </div>
+          <p className="font-semibold text-white bg-black w-fit px-5 py-1 rounded-full text-sm mt-3">
+            @AuthorName
+          </p>
         </div>
       </div>
     </div>
