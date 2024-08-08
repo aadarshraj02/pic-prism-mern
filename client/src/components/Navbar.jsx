b5777c49c22abb3c7bc077143b2500dc78e1cd82
aadarshraj02 <aadarshraazz007@gmail.com>
2024-08-08 19:14:18 +0530

    added route on li items

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,7 +1,13 @@
 import { Link } from "react-router-dom";
 
 const Navbar = () => {
-  const liItems = ["About", "Contact", "Login", "Sign up"];
+  const liItems = [
+    { name: "About", route: "/about" },
+    { name: "Contact", route: "/contact" },
+    { name: "Login", route: "/login" },
+    { name: "Sign up", route: "/signup" },
+  ];
+
   return (
     <nav className="flex flex-col sm:flex-row justify-between items-start sm:items-center p-5 fixed top-0 left-0 right-0 shadow-md gap-1 sm:gap-0 z-30">
       <div className="flex justify-between items-center">
@@ -9,10 +15,10 @@ const Navbar = () => {
           PicPrism
         </Link>
       </div>
-      <ul className="flex items-center gap-5 text-lg font-semibold text-gray-500 m">
+      <ul className="flex items-center gap-5 text-lg font-semibold text-gray-500">
         {liItems.map((item, i) => (
           <li className="hover:text-black cursor-pointer sm:p-2" key={i}>
-            {item}
+            <Link to={item.route}>{item.name}</Link>
           </li>
         ))}
       </ul>
