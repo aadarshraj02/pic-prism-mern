d9dcf47aa3f05c203a98b4ecd2f7b9caaddc164e
aadarshraj02 <aadarshraazz007@gmail.com>
2024-08-08 21:01:53 +0530

    added image

--- client/src/components/PhotoGallery.jsx
+++ client/src/components/PhotoGallery.jsx
@@ -2,6 +2,14 @@ const PhotoGallery = () => {
   return (
     <div className="my-20 bg-white flex flex-col justify-center items-center">
       <h3 className="text-3xl font-semibold my-14">Photos</h3>
+      <div>
+        <div>
+          <img
+            src="https://plus.unsplash.com/premium_photo-1722586209835-46ae3736ec12?q=80&w=1535&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            alt="image"
+          />
+        </div>
+      </div>
     </div>
   );
 };
