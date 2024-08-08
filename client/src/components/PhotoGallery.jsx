303c80362beb8d4bfd3d6611b380c99a5d443590
aadarshraj02 <aadarshraazz007@gmail.com>
2024-08-08 21:32:55 +0530

    made responsive according to different devices

--- client/src/components/PhotoGallery.jsx
+++ client/src/components/PhotoGallery.jsx
@@ -4,7 +4,18 @@ const PhotoGallery = () => {
   return (
     <div className="my-20 bg-white flex flex-col justify-center items-center">
       <h3 className="text-3xl font-semibold my-14">Photos</h3>
-      <div className="grid grid-cols-1 sm:grid-cols-3 gap-5 mb-20">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-5 mb-20">
+        <ImageCard />
+        <ImageCard />
+        <ImageCard />
+        <ImageCard />
+        <ImageCard />
+        <ImageCard />
+        <ImageCard />
+        <ImageCard />
+        <ImageCard />
+        <ImageCard />
+        <ImageCard />
         <ImageCard />
       </div>
     </div>
