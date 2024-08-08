c72963c2de5e1de25049cb692bf4080b337f7c77
aadarshraj02 <aadarshraazz007@gmail.com>
2024-08-08 21:30:44 +0530

    imported image card

--- client/src/components/PhotoGallery.jsx
+++ client/src/components/PhotoGallery.jsx
@@ -1,11 +1,11 @@
-
+import ImageCard from "./ImageCard";
 
 const PhotoGallery = () => {
   return (
     <div className="my-20 bg-white flex flex-col justify-center items-center">
       <h3 className="text-3xl font-semibold my-14">Photos</h3>
       <div className="grid grid-cols-1 sm:grid-cols-3 gap-5 mb-20">
-      
+        <ImageCard />
       </div>
     </div>
   );
