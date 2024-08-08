5db11f7f75e5a356ddb021a6629856aaad900181
aadarshraj02 <aadarshraazz007@gmail.com>
2024-08-08 19:37:53 +0530

    added search icon

--- client/src/components/HeroSection.jsx
+++ client/src/components/HeroSection.jsx
@@ -1,3 +1,5 @@
+import { IoIosSearch } from "react-icons/io";
+
 const HeroSection = () => {
   return (
     <div className="sm:w-[60vw] h-[20vh] overflow-clip sm:rounded-3xl mx-auto flex justify-center items-center">
@@ -9,6 +11,7 @@ const HeroSection = () => {
           placeholder="Search your asset..."
           className="py-5 px-3 w-[80vw] text-xl sm:text-3xl mx-auto outline-none border-b-2"
         />
+        <IoIosSearch />
       </form>
     </div>
   );
