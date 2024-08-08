3a4a120fc9b4fd34e88abc8085ed06456e0f4b7f
aadarshraj02 <aadarshraazz007@gmail.com>
2024-08-08 18:51:27 +0530

    added nav structure

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,5 +1,20 @@
+import { Link } from "react-router-dom";
+
 const Navbar = () => {
-  return <div></div>;
+  return (
+    <div>
+      <div>
+        <Link>PicPrism</Link>
+        <h1>hello</h1>
+      </div>
+      <ul>
+        <li>About</li>
+        <li>Contact</li>
+        <li>Login</li>
+        <li>Sign up</li>
+      </ul>
+    </div>
+  );
 };
 
 export default Navbar;
