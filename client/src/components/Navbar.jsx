bfa70b5790a59a459476d5ef8800cd4cb5021be6
aadarshraj02 <aadarshraazz007@gmail.com>
2024-08-08 19:56:19 +0530

    applied bg-color on nav

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -9,7 +9,7 @@ const Navbar = () => {
   ];
 
   return (
-    <nav className="flex flex-col sm:flex-row justify-between items-start sm:items-center p-5 fixed top-0 left-0 right-0 shadow-md gap-1 sm:gap-0 z-30">
+    <nav className="flex flex-col sm:flex-row justify-between items-start sm:items-center p-5 fixed top-0 left-0 right-0 shadow-md gap-1 sm:gap-0 z-30 bg-white">
       <div className="flex justify-between items-center">
         <Link to="/" className="font-bold text-3xl">
           PicPrism
