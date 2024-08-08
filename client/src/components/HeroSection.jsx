cd6f6e3b7a420bf044f3f30c91a25e5e8ee5b549
aadarshraj02 <aadarshraazz007@gmail.com>
2024-08-08 19:20:51 +0530

    formatted code

--- client/src/components/HeroSection.jsx
+++ client/src/components/HeroSection.jsx
@@ -2,7 +2,13 @@ const HeroSection = () => {
   return (
     <div>
       <form className="sm:w-[60vw] h-[20vh] overflow-clip sm:rounded-3xl mx-auto flex justify-center items-center">
-        <input type="search" id="search" name="search" className="" />
+        <input
+          type="search"
+          id="search"
+          name="search"
+          placeholder="Search your asset..."
+          className=""
+        />
       </form>
     </div>
   );
