6d5922128955aca07a75f72c1ddd5ee6243e3f1a
aadarshraj02 <aadarshraazz007@gmail.com>
2024-08-08 14:32:10 +0530

    added browser routes and route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,15 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 const App = () => {
-  return <div></div>;
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 };
 
 export default App;
