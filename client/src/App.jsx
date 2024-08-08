f34ce42601535662d60ec502453d618d72839d31
aadarshraj02 <aadarshraazz007@gmail.com>
2024-08-08 14:54:02 +0530

    imported and added sellerdashboard

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,7 @@ import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import BuyerDashboard from "./pages/BuyerDashboard";
+import SellerDashboard from "./pages/SellerDashboard";
 
 const App = () => {
   return (
@@ -13,6 +14,7 @@ const App = () => {
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
           <Route path="/buyer/profile" element={<BuyerDashboard />} />
+          <Route path="/seller/profile" element={<SellerDashboard />} />
         </Routes>
       </BrowserRouter>
     </>
