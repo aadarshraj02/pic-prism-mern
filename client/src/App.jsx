077b762bd865ff8ba7d6941a3807a5d512f49072
aadarshraj02 <aadarshraazz007@gmail.com>
2024-08-08 14:35:18 +0530

    imported home and passed as element in route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,12 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
 
 const App = () => {
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route path="/" />
+          <Route path="/" element={<Home />} />
         </Routes>
       </BrowserRouter>
     </>
