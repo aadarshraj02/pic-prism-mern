d6adab44f69616fb549a89c2cc8bafa31b3ddddd
aadarshraj02 <aadarshraazz007@gmail.com>
2024-08-08 19:10:33 +0530

    styled nav

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,19 +1,22 @@
 import { Link } from "react-router-dom";
 
 const Navbar = () => {
+  const liItems = ["About", "Contact", "Login", "Sign up"];
   return (
-    <div>
-      <div>
-        <Link>PicPrism</Link>
-        <h1>hello</h1>
+    <nav className="flex flex-col sm:flex-row justify-between items-start sm:items-center p-5 fixed top-0 left-0 right-0 shadow-md gap-1 sm:gap-0 z-30">
+      <div className="flex justify-between items-center">
+        <Link to="/" className="font-bold text-3xl">
+          PicPrism
+        </Link>
       </div>
-      <ul>
-        <li>About</li>
-        <li>Contact</li>
-        <li>Login</li>
-        <li>Sign up</li>
+      <ul className="flex items-center gap-5 text-lg font-semibold text-gray-500 m">
+        {liItems.map((item, i) => (
+          <li className="hover:text-black cursor-pointer sm:p-2" key={i}>
+            {item}
+          </li>
+        ))}
       </ul>
-    </div>
+    </nav>
   );
 };
 
