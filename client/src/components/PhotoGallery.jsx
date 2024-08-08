4a125eae137c3fe268785ac258d05c1ca6543e64
aadarshraj02 <aadarshraazz007@gmail.com>
2024-08-08 21:28:07 +0530

    made responsive

--- client/src/components/PhotoGallery.jsx
+++ client/src/components/PhotoGallery.jsx
@@ -5,7 +5,7 @@ const PhotoGallery = () => {
   return (
     <div className="my-20 bg-white flex flex-col justify-center items-center">
       <h3 className="text-3xl font-semibold my-14">Photos</h3>
-      <div>
+      <div className="grid grid-cols-1 sm:grid-cols-3 gap-5 mb-20">
         <div className="rounded-lg bg-white shadow-lg p-2">
           <div className="w-full h-[200px] overflow-hidden rounded-2xl ">
             <img
