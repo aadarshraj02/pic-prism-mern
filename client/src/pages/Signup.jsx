1cf0e0360dc41c3d0ec93d93a0fcf693ad40ea0f
aadarshraj02 <aadarshraazz007@gmail.com>
2024-08-09 10:36:04 +0530

    added label and input for password

--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -29,10 +29,25 @@ const Signup = () => {
               Email Address
             </label>
             <input
-              type="text"
+              type="email"
               name="email"
               id="email"
               placeholder="your@email.com"
+              className="shadow-md rounded-md w-full px-3 py-2 border-gray-300 focus:outline-none focus:ring-black focus:border-black"
+            />
+          </div>
+          <div className="mb-4">
+            <label
+              htmlFor="password"
+              className="block text-sm font-medium text-gray-700 mb-2"
+            >
+              Password
+            </label>
+            <input
+              type="password"
+              name="password"
+              id="password"
+              placeholder="Enter any Password"
               className="shadow-md rounded-md w-full px-3 py-2 border-gray-300 focus:outline-none focus:ring-black focus:border-black"
             />
           </div>
