cf8dbd4c0bf4d2c01c7f9b3eb7eb145912ef1d9d
aadarshraj02 <aadarshraazz007@gmail.com>
2024-08-10 00:02:33 +0530

    passed route dynamically

--- server/index.js
+++ server/index.js
@@ -1,10 +1,15 @@
 const express = require("express");
 require("dotenv").config();
+const { readdirSync } = require("fs");
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.get("/", (req, res) => {
   res.send("<center><h1>Server is running</h1></center>");
+});
+
+readdirSync("./routes").map((route) => {
+  app.arguments("/api", require(`./routes/${route}`));
 });
 
 app.listen(port, () => {
