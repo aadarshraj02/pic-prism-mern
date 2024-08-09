9a753e0f12f7035c84e0d4cf6c8533cf48d039d9
aadarshraj02 <aadarshraazz007@gmail.com>
2024-08-09 23:30:53 +0530

    passed port as env

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
+require("dotenv").config();
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 app.get("/", (req, res) => {
   res.send("<center><h1>Server is running</h1></center>");
