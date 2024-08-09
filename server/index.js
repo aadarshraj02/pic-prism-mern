136d5b5deb099d9afa56e648e104ac94b9ab6804
aadarshraj02 <aadarshraazz007@gmail.com>
2024-08-09 23:07:48 +0530

    added req to check server is running or not

--- server/index.js
+++ server/index.js
@@ -1,6 +1,11 @@
 const express = require("express");
 const app = express();
 const port = 5000;
+
+app.get("/", (req, res) => {
+  res.send("<center><h1>Server is running</h1></center>");
+});
+
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
