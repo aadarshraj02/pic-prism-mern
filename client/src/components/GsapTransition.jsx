f62d51fe0103655a5d1866035efff20e08d3f4be
aadarshraj02 <aadarshraazz007@gmail.com>
2024-08-09 21:49:03 +0530

    added imports

--- client/src/components/GsapTransition.jsx
+++ client/src/components/GsapTransition.jsx
@@ -1,5 +1,22 @@
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import BuyerDashboard from "./pages/BuyerDashboard";
+import SellerDashboard from "./pages/SellerDashboard";
+
 const GsapTransition = () => {
-  return <div></div>;
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/buyer/profile" element={<BuyerDashboard />} />
+        <Route path="/seller/profile" element={<SellerDashboard />} />
+      </Routes>
+    </div>
+  );
 };
 
 export default GsapTransition;
