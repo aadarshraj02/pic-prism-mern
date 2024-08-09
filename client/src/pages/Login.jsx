3a252df706bc9b22bafecf7c258542c102707bb4
aadarshraj02 <aadarshraazz007@gmail.com>
2024-08-09 11:04:13 +0530

    made login page

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,5 +1,56 @@
+import { Link } from "react-router-dom";
+
 const Login = () => {
-  return <div></div>;
+  return (
+    <div className="mt-20 sm:mt-10 h-[90vh] flex items-center justify-center w-full ">
+      <div className="bg-white shadow-md rounded-3xl px-5 py-6 w-full sm:w-[27vw]">
+        <h1 className="text-2xl font-bold text-center mb-4">
+          Let&apos;s Connect
+        </h1>
+        <form>
+          <div className="mb-4">
+            <label
+              htmlFor="email"
+              className="block text-sm font-medium text-gray-700 mb-2"
+            >
+              Email Address
+            </label>
+            <input
+              type="email"
+              name="email"
+              id="email"
+              placeholder="your@email.com"
+              className="shadow-md rounded-md w-full px-3 py-2 border-gray-300 focus:outline-none focus:ring-black focus:border-black"
+            />
+          </div>
+          <div className="mb-4">
+            <label
+              htmlFor="password"
+              className="block text-sm font-medium text-gray-700 mb-2"
+            >
+              Password
+            </label>
+            <input
+              type="password"
+              name="password"
+              id="password"
+              placeholder="Enter any Password"
+              className="shadow-md rounded-md w-full px-3 py-2 border-gray-300 focus:outline-none focus:ring-black focus:border-black"
+            />
+          </div>
+          <div className="flex items-center justify-between mb-4">
+            <Link className="text-xs text-gray-500">Forgot Password?</Link>
+            <Link className=" text-xs text-black" to="/signup">
+              Don&apos;t have an account Signup?
+            </Link>
+          </div>
+          <button className="w-full py-2 px-4 rounded-md shadow-md text-sm font-medium text-white bg-black">
+            Login
+          </button>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
