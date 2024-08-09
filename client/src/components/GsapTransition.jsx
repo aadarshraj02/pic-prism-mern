5361f96027794b10f03b2f92a23509407691b078
aadarshraj02 <aadarshraazz007@gmail.com>
2024-08-09 22:07:09 +0530

    added gsap transition

--- client/src/components/GsapTransition.jsx
+++ client/src/components/GsapTransition.jsx
@@ -1,14 +1,33 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, useLocation } from "react-router-dom";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import BuyerDashboard from "./pages/BuyerDashboard";
 import SellerDashboard from "./pages/SellerDashboard";
+import gsap from "gsap";
+import { useEffect, useRef } from "react";
 
 const GsapTransition = () => {
+  const nodeRef = useRef(null);
+  const location = useLocation();
+  useEffect(() => {
+    if (nodeRef.current) {
+      gsap.fromTo(
+        nodeRef.current,
+        {
+          opacity: 0,
+        },
+        {
+          opacity: 1,
+          duration: 1,
+        }
+      );
+    }
+  }, [location]);
+
   return (
-    <div>
-      <Routes>
+    <div ref={nodeRef}>
+      <Routes location={location}>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
