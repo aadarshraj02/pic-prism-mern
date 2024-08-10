390930e65d1b845352e4e60edbec7505645f3d8d
aadarshraj02 <aadarshraazz007@gmail.com>
2024-08-10 19:40:39 +0530

    added cors as require

--- server/index.js
+++ server/index.js
@@ -2,10 +2,13 @@ const express = require("express");
 require("dotenv").config();
 const { readdirSync } = require("fs");
 const { connectDb } = require("./connection");
+const cors = require("cors");
+
 const app = express();
 const port = process.env.PORT || 5000;
 
 connectDb();
+app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
