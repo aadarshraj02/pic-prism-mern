cd0c5985117382754368d112b3977ad5f2fe99e2
aadarshraj02 <aadarshraazz007@gmail.com>
2024-08-10 19:31:49 +0530

    added signup

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -5,6 +5,25 @@ const bcrypt = require("bcrypt");
 const signup = async (req, res) => {
   const { username, email, password, accountType } = req.body;
   try {
+    let user = await User.findOne({ username });
+    if (user) {
+      return res.status(400).json({
+        success: false,
+        message: "Username already used ",
+      });
+    }
+    const securePassword = await bcrypt.hash(password, 10);
+    user = new User({
+      username,
+      email,
+      password: securePassword,
+      accountType,
+    });
+    await user.save();
+    return res.status(201).json({
+      success: true,
+      message: "User created successfully",
+    });
   } catch (error) {
     return res.status(500).json({
       success: false,
