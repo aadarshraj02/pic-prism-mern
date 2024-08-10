2fe3f8bbe3569848b36c45a4ed9e0ac943d2cc06
aadarshraj02 <aadarshraazz007@gmail.com>
2024-08-10 21:35:39 +0530

    fixed issue with refresh token

--- server/controllers/refreshToken.js
+++ server/controllers/refreshToken.js
@@ -1,7 +1,6 @@
 const jwt = require("jsonwebtoken");
 
-const generateRefreshToken = (user) => {
+const generateRefreshToken = (user) =>
   jwt.sign(user, process.env.REFRESH_TOKEN_SECRET, { expiresIn: "1d" });
-};
 
 module.exports = { generateRefreshToken };
