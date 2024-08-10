fa29475bc9f37deca7c47279b8c3c7e7353472db
aadarshraj02 <aadarshraazz007@gmail.com>
2024-08-10 19:03:10 +0530

    added try catch to handle error while getting res from body

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,6 +1,16 @@
 const User = require("../model/User");
 const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
 
+const signup = async (req, res) => {
+  const { username, email, password, accountType } = req.body;
+  try {
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
 const login = async (req, res) => {};
-const signup = async (req, res) => {};
 module.exports = { login, signup };
