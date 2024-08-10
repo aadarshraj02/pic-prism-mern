53ccad3c0d7a5ae21bd593c4966e40bec6265d1d
aadarshraj02 <aadarshraazz007@gmail.com>
2024-08-10 15:13:42 +0530

    called signup

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,6 +1,7 @@
-const { login } = require("../controllers/authController");
+const { login, signup } = require("../controllers/authController");
 
 const router = require("express").Router();
 router.post("/login", login);
+router.post("/signup", signup);
 
 module.exports = router;
