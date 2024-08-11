8256546d4a0c4d39ebeae458c5d43b27a4e99b6f
aadarshraj02 <aadarshraazz007@gmail.com>
2024-08-11 23:35:30 +0530

    displayed welcome message dynamically

--- client/src/components/DashboardHeader.jsx
+++ client/src/components/DashboardHeader.jsx
@@ -2,10 +2,17 @@ import { useSelector } from "react-redux";
 
 const DashboardHeader = () => {
   const author = useSelector((state) => state.auth.author);
+  const role = useSelector((state) => state.auth.role);
   return (
     <div>
       <div className="my-5 mx-8">
-        <h1>Hello, {author?.charAt(0).toUpperCase() + author?.slice(1)}</h1>
+        <h1 className="text-3xl font-bold">
+          Hello, {author?.charAt(0).toUpperCase() + author?.slice(1)}
+        </h1>
+        <p>
+          Welcome to your {role.charAt(0).toUpperCase() + role.slice(1)}{" "}
+          Dashbaord
+        </p>
       </div>
     </div>
   );
