2e3c0574aa3cb72b3fa96cba66c09eeb673f475d
aadarshraj02 <aadarshraazz007@gmail.com>
2024-08-11 21:10:07 +0530

    showed author's firct char as in a circle

--- client/src/components/DashboardSidebar.jsx
+++ client/src/components/DashboardSidebar.jsx
@@ -1,5 +1,16 @@
+import { useSelector } from "react-redux";
+
 const DashboardSidebar = () => {
-  return <div></div>;
+  const author = useSelector((state) => state.auth.author);
+  return (
+    <div className="flex text-lg font-semibold bg-white shadow-lg flex-col gap-2 w-fit min-h-screen p-3 list-none justify-between items-center">
+      <div>
+        <div className="bg-black my-5 w-fit rounded-full px-6 py-4 text-white">
+          {author.charAt(0).toUpperCase()}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default DashboardSidebar;
