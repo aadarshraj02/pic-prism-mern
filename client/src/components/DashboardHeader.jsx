76436d7f6aea17888cd143237fdbb0f76fa47c3c
aadarshraj02 <aadarshraazz007@gmail.com>
2024-08-11 23:28:50 +0530

    fixed issue in displaying author

--- client/src/components/DashboardHeader.jsx
+++ client/src/components/DashboardHeader.jsx
@@ -1,11 +1,11 @@
 import { useSelector } from "react-redux";
 
 const DashboardHeader = () => {
-  const author = useSelector((state) => state.nav.author);
+  const author = useSelector((state) => state.auth.author);
   return (
     <div>
       <div className="my-5 mx-8">
-        <h1>Hello, {author.charAt(0).toUpperCase() + author.slice(1)}</h1>
+        <h1>Hello, {author?.charAt(0).toUpperCase() + author?.slice(1)}</h1>
       </div>
     </div>
   );
