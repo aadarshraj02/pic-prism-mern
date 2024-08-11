4830a92cb5161ddf80e143dc5129c03210c29a02
aadarshraj02 <aadarshraazz007@gmail.com>
2024-08-11 09:37:38 +0530

    added verify token and added if token not found return

---
+++ server/middlewares/verifyToken.js
@@ -0,0 +1,12 @@
+const jwt = require("jsonwebtoken");
+
+const verifyToken = async (req, res, next) => {
+  const authHeader = req.header("Authorization");
+  const token = authHeader && authHeader.split(" ")[1];
+
+  if (!token)
+    return res.status(401).json({
+      success: false,
+      message: "Unauthorized",
+    });
+};
