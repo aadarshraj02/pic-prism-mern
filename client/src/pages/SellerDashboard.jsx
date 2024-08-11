73a3a7f5ecc9e7d24ec3a33c2e87dc86e19e8ff8
aadarshraj02 <aadarshraazz007@gmail.com>
2024-08-11 22:54:54 +0530

    imported photomanagement.jsx

--- client/src/pages/SellerDashboard.jsx
+++ client/src/pages/SellerDashboard.jsx
@@ -1,9 +1,11 @@
 import DashboardSidebar from "../components/DashboardSidebar";
+import PhotoManagement from "../components/seller/PhotoManagement";
 
 const SellerDashboard = () => {
   return (
     <div>
       <DashboardSidebar />
+      <PhotoManagement />
     </div>
   );
 };
