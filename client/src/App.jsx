9335b2d1a6f27f444412406e3270288ed572ac2a
aadarshraj02 <aadarshraazz007@gmail.com>
2024-08-11 17:48:57 +0530

    imported store and added provider

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,18 @@
 import { BrowserRouter } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import GsapTransition from "./components/GsapTransition";
+import { Provider } from "react-redux";
+import { store } from "../store/store";
 
 const App = () => {
   return (
     <>
-      <BrowserRouter>
-        <Navbar />
-        <GsapTransition />
-      </BrowserRouter>
+      <Provider store={store}>
+        <BrowserRouter>
+          <Navbar />
+          <GsapTransition />
+        </BrowserRouter>
+      </Provider>
     </>
   );
 };
