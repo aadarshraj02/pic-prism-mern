b43a9ffd65c61010c41c6e4e2750583870c71e4d
aadarshraj02 <aadarshraazz007@gmail.com>
2024-08-11 18:34:56 +0530

    imported dashboard sidebar

--- client/src/pages/SellerDashboard.jsx
+++ client/src/pages/SellerDashboard.jsx
@@ -1,5 +1,11 @@
+import DashboardSidebar from "../components/DashboardSidebar";
+
 const SellerDashboard = () => {
-  return <div></div>;
+  return (
+    <div>
+      <DashboardSidebar />
+    </div>
+  );
 };
 
 export default SellerDashboard;
