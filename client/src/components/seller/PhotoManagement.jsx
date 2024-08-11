c0c8669e84ce4f2c97880a936c5b6d97aae17e65
aadarshraj02 <aadarshraazz007@gmail.com>
2024-08-12 00:06:51 +0530

    imported imageadd.jsx

--- client/src/components/seller/PhotoManagement.jsx
+++ client/src/components/seller/PhotoManagement.jsx
@@ -1,10 +1,12 @@
 import DashboardHeader from "../DashboardHeader";
+import ImageAdd from "../ImageAdd";
 
 const PhotoManagement = () => {
   return (
     <div className="flex flex-col sm:flex-row">
       <div>
-        <DashboardHeader/>
+        <DashboardHeader />
+        <ImageAdd />
       </div>
     </div>
   );
