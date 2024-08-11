ef6637cdb22fa527733df6eaf75bd8a63328ea28
aadarshraj02 <aadarshraazz007@gmail.com>
2024-08-11 23:18:41 +0530

    dispalyed authorname

--- client/src/components/DashboardHeader.jsx
+++ client/src/components/DashboardHeader.jsx
@@ -1,5 +1,14 @@
+import { useSelector } from "react-redux";
+
 const DashboardHeader = () => {
-  return <div></div>;
+  const author = useSelector((state) => state.nav.author);
+  return (
+    <div>
+      <div className="my-5 mx-8">
+        <h1>Hello, {author.charAt(0).toUpperCase() + author.slice(1)}</h1>
+      </div>
+    </div>
+  );
 };
 
 export default DashboardHeader;
