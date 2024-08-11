f8a3e5428da957248d7704455412f55d56e63649
aadarshraj02 <aadarshraazz007@gmail.com>
2024-08-11 21:18:32 +0530

    added logout button

--- client/src/components/DashboardSidebar.jsx
+++ client/src/components/DashboardSidebar.jsx
@@ -1,15 +1,21 @@
 import { useSelector } from "react-redux";
+import { AiOutlineLogout } from "react-icons/ai";
 
 const DashboardSidebar = () => {
   const author = useSelector((state) => state.auth.author);
   return (
-    <div className="flex text-lg font-semibold bg-white shadow-lg flex-col gap-2 w-fit min-h-screen p-3 list-none justify-between items-center">
+    <nav className="flex text-lg font-semibold bg-white shadow-lg flex-col gap-2 w-fit min-h-screen p-3 list-none justify-between items-center">
       <div>
         <div className="bg-black my-5 w-fit rounded-full px-6 py-4 text-white">
           {author.charAt(0).toUpperCase()}
         </div>
+        <div></div>
       </div>
-    </div>
+      <button className="flex items-center justify-start gap-2 w-full rounded-lg px-2 hover:bg-black hover:text-white transition-all ease-linear duration-300 hover:scale-105">
+        Logout
+        <AiOutlineLogout />
+      </button>
+    </nav>
   );
 };
 
