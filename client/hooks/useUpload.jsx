c3cf75eb7e448340b45e36b5b527687c8a593644
aadarshraj02 <aadarshraazz007@gmail.com>
2024-08-12 19:41:55 +0530

    added upload function to add image on cloudinary

--- client/hooks/useUpload.jsx
+++ client/hooks/useUpload.jsx
@@ -1,5 +1,34 @@
 import axios from "axios";
 
-const useUpload = async ({ image, onUploadProgress }) => {};
+const useUpload = async ({ image, onUploadProgress }) => {
+  const upload = async () => {
+    try {
+      const formData = new FormData();
+      formData.append("file", image);
+      formData.append(
+        "upload_preset",
+        import.meta.env.VITE_CLOUDINARY_UPLOAD_PRESET
+      );
+      formData.append("api_key", import.meta.env.VITE_CLOUDINARY_API_KEY);
+      const config = {
+        headers: {
+          "Content-type": "multipart/form-data",
+        },
+        onUploadProgress,
+        withCredentials: false,
+      };
+      const res = await axios.post(
+        `https://api.cloudinary.com/v1_1/${
+          import.meta.env.VITE_CLOUDINARY_CLOUD_NAME
+        }/image/upload`
+      );
+      const data = await res.data;
+      if (!data) return console.log("Image Upload failed");
+      return data;
+    } catch (error) {
+      return error.message;
+    }
+  };
+};
 
 export default useUpload;
