6bc4c628c397ba9bf3a6adbb43e4084ce6911b0c
aadarshraj02 <aadarshraazz007@gmail.com>
2024-08-12 23:30:31 +0530

    added routes  for getAllPosts,createPost and getMyPost

--- server/routes/postroutes.js
+++ server/routes/postroutes.js
@@ -1,2 +1,13 @@
 const router = require("express").Router();
+const { verifyToken } = require("../middlewares/verifyToken");
+const {
+  createPost,
+  getAllPosts,
+  getMyPosts,
+} = require("../controllers/postController");
+
+router.post("/post/create", verifyToken, createPost);
+router.get("/post/getAll", getAllPosts);
+router.get("/post/myPost", verifyToken, getMyPosts);
+
 module.exports = router;
