817d8cb570db9d5ce83efe6de3fa0e509588b16c
aadarshraj02 <aadarshraazz007@gmail.com>
2024-08-12 11:11:07 +0530

    added div and h2 for title of form

--- client/src/components/ImageAdd.jsx
+++ client/src/components/ImageAdd.jsx
@@ -1,5 +1,9 @@
 const ImageAdd = () => {
-  return <div></div>;
+  return (
+    <div className="p-5 mx-9 rounded-2xl shadow-md bg-white">
+      <h2 className="text-xl font-bold">Add New Product</h2>
+    </div>
+  );
 };
 
 export default ImageAdd;
