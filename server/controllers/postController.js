495f62d6ff3ba7a3cd8ba9748c00246e60e2e2c7
aadarshraj02 <aadarshraazz007@gmail.com>
2024-08-12 22:59:43 +0530

    added function to get all posts

--- server/controllers/postController.js
+++ server/controllers/postController.js
@@ -40,3 +40,23 @@ const createPost = async (req, res) => {
     });
   }
 };
+
+const getAllPosts = async (req, res) => {
+  try {
+    const posts = await Post.find({});
+    if (posts.length === 0)
+      return res.status(404).json({
+        success: false,
+        message: "No Post Found",
+      });
+    return res.status(200).json({
+      success: true,
+      data: posts,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
