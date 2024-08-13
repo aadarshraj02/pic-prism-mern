0ba5a12a24ca8a4a2c2126906babbd2c24e40514
aadarshraj02 <aadarshraazz007@gmail.com>
2024-08-13 17:53:46 +0530

    removed postSlice.js

--- client/store/store.js
+++ client/store/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authSlice from "./slices/authSlice";
 import navSlice from "./slices/navSlice";
+import postsSlice from "./slices/postSlice";
 
 export const store = configureStore({
   reducer: {
     auth: authSlice,
     nav: navSlice,
+    posts: postsSlice,
   },
 });
