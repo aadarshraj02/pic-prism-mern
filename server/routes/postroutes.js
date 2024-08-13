cd2337b8716b171f051cd4eda05dbc8a9d94ab3e
aadarshraj02 <aadarshraazz007@gmail.com>
2024-08-14 00:34:20 +0530

    added search post router

--- server/routes/postroutes.js
+++ server/routes/postroutes.js
@@ -5,11 +5,13 @@ const {
   getAllPosts,
   getMyPosts,
   deletePost,
+  searchPost,
 } = require("../controllers/postController");
 
 router.post("/post/create", verifyToken, createPost);
 router.get("/post/getAll", getAllPosts);
 router.get("/post/myPosts", verifyToken, getMyPosts);
 router.delete("/post/delete/:id", verifyToken, deletePost);
+router.get("/posts/search", searchPost);
 
 module.exports = router;
