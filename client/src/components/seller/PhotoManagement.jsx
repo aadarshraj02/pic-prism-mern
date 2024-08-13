32d979e2f4dcbfc0244ac500647e733aabf01233
aadarshraj02 <aadarshraazz007@gmail.com>
2024-08-13 20:28:05 +0530

    updated icons fixed error with data

--- client/src/components/seller/PhotoManagement.jsx
+++ client/src/components/seller/PhotoManagement.jsx
@@ -1,13 +1,13 @@
-import { useDispatch, useSelector } from "react-redux";
+import React, { useEffect } from "react";
 import DashboardHeader from "../DashboardHeader";
 import ImageAdd from "../ImageAdd";
+import { useDispatch, useSelector } from "react-redux";
 import toast from "react-hot-toast";
 import { logout } from "../../../store/slices/authSlice";
 import { setMyPosts } from "../../../store/slices/postSlice";
 import axios from "axios";
-import { useEffect } from "react";
-import ImageCard from "../../components/ImageCard";
-import { FaEdit } from "react-icons/fa";
+import ImageCard from "../ImageCard";
+import { BiSolidMessageSquareEdit } from "react-icons/bi";
 import { MdDelete } from "react-icons/md";
 
 const PhotoManagement = () => {
@@ -18,14 +18,16 @@ const PhotoManagement = () => {
     try {
       if (posts.length > 0) return;
       const res = await axios.get(
-        import.meta.env.VITE_API_URL + "post/myPosts",
+        import.meta.env.VITE_API_URL + "/post/myPosts",
         {
           headers: {
-            Authorization: "Bearer" + localStorage.get("accessToken"),
+            Authorization: "Bearer " + localStorage.getItem("accessToken"),
           },
         }
       );
+
       const { data } = await res.data;
+      console.log(data);
       dispatch(setMyPosts(data));
     } catch (error) {
       toast.error(error.response.data.message);
@@ -44,19 +46,33 @@ const PhotoManagement = () => {
         <ImageAdd />
       </div>
 
-      <div className="grid grid-cols-1 sm:grid-cols-3 gap-5 mt-5 bg-transparent sm:bg-white p-5 w-[90vw] sm:w-[55vw] sm:h-[95vh] sm:overflow-y-scroll rounded-lg mx-auto sm:mx-0">
-        {posts?.map(({ _id, title, image, author, price }) => (
-          <ImageCard
-            key={_id}
-            id={_id}
-            title={title}
-            img={image}
-            author={author}
-            price={price}
-            icon1={<FaEdit />}
-            icon2={<MdDelete />}
-          />
-        ))}
+      {/* Section where all the images are displayed */}
+      <div className="gird gird-cols-1 sm:grid-cols-3 gap-5 mt-5 bg-transparent sm:bg-white p-5 w-[90vw] sm:w-[55vw] sm:h-[95vh] sm:overflow-y-scroll rounded-lg mx-auto sm:mx-0">
+        {posts.length &&
+          posts?.map(({ _id, title, image, author, price }) => {
+            return (
+              <ImageCard
+                key={_id}
+                id={_id}
+                title={title}
+                img={image}
+                author={author}
+                price={price}
+                icon1={
+                  <BiSolidMessageSquareEdit
+                    title="Edit"
+                    className="text-3xl text-black cursor-pointer hover:scale-110 transition-all ease-linear duration-300"
+                  />
+                }
+                icon2={
+                  <MdDelete
+                    title="Delete"
+                    className="text-3xl text-red-500 cursor-pointer hover:scale-110 transition-all ease-linear duration-300"
+                  />
+                }
+              />
+            );
+          })}
       </div>
     </div>
   );
