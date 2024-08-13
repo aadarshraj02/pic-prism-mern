0bee0eb2d315a7b118dbf699c76d0c8e8d023785
aadarshraj02 <aadarshraazz007@gmail.com>
2024-08-13 21:02:37 +0530

    fixed error with getting posts

--- server/controllers/postController.js
+++ server/controllers/postController.js
@@ -6,38 +6,26 @@ const createPost = async (req, res) => {
   const authorAccountType = req.accountType;
 
   if (authorAccountType == "buyer") {
-    return res.status(403).json({
-      success: false,
-      message: "Forbidden,only sellers can post",
-    });
+    return res
+      .status(403)
+      .json({ success: false, message: "Forbidden, only sellers can post" });
   }
 
   const { title, author, price, image, publicId } = req.body;
 
   try {
-    const post = new Post({
-      title,
-      author,
-      price,
-      image,
-      publicId,
-      authorId,
-    });
+    const post = new Post({ title, author, price, image, publicId, authorId });
     await post.save();
 
     await User.findByIdAndUpdate(authorId, {
       $push: { uploads: post._id },
     });
-    return res.status(201).json({
-      success: true,
-      message: "Post Created Successfully",
-      post,
-    });
+
+    return res
+      .status(201)
+      .json({ success: true, message: "Post created successfully", post });
   } catch (error) {
-    return res.status(500).json({
-      success: false,
-      message: error.message,
-    });
+    return res.status(500).json({ success: false, message: error.message });
   }
 };
 
@@ -45,19 +33,13 @@ const getAllPosts = async (req, res) => {
   try {
     const posts = await Post.find({});
     if (posts.length === 0)
-      return res.status(404).json({
-        success: false,
-        message: "No Post Found",
-      });
-    return res.status(200).json({
-      success: true,
-      data: posts,
-    });
+      return res
+        .status(404)
+        .json({ success: false, message: "No posts found" });
+
+    return res.status(200).json({ success: true, data: posts });
   } catch (error) {
-    return res.status(500).json({
-      success: false,
-      message: error.message,
-    });
+    return res.status(500).json({ success: false, message: error.message });
   }
 };
 
@@ -67,33 +49,25 @@ const getMyPosts = async (req, res) => {
   try {
     if (authorAccountType === "buyer") {
       const { purchased } = await User.findById(authorId).populate("purchased");
-
+      console.log(purchased);
       if (!purchased)
-        return res.status(404).json({
-          success: false,
-          message: "No Post Found",
-        });
-      return res.status(200).json({
-        success: true,
-        data: purchased,
-      });
+        return res
+          .status(404)
+          .json({ success: false, message: "No posts found" });
+
+      return res.status(200).json({ success: true, data: purchased });
     } else {
       const { uploads } = await User.findById(authorId).populate("uploads");
       if (!uploads)
-        return res.status(404).json({
-          success: false,
-          data: uploads,
-        });
-      return res.status(200).json({
-        success: true,
-        message: "Post Found",
-      });
+        return res
+          .status(404)
+          .json({ success: false, message: "No posts found" });
+      return res.status(200).json({ success: true, data: uploads });
     }
   } catch (error) {
-    return res.status(500).json({
-      success: false,
-      message: error.message,
-    });
+    return res
+      .status(500)
+      .json({ success: false, message: "Internal server error" });
   }
 };
 
