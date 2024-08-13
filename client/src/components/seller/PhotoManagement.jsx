adce3342fb46ce3021c19480d51ba9dc89324192
aadarshraj02 <aadarshraazz007@gmail.com>
2024-08-13 20:10:53 +0530

    imported image card added id title image author price delete icon and edit icon

--- client/src/components/seller/PhotoManagement.jsx
+++ client/src/components/seller/PhotoManagement.jsx
@@ -6,6 +6,9 @@ import { logout } from "../../../store/slices/authSlice";
 import { setMyPosts } from "../../../store/slices/postSlice";
 import axios from "axios";
 import { useEffect } from "react";
+import ImageCard from "../../components/ImageCard";
+import { FaEdit } from "react-icons/fa";
+import { MdDelete } from "react-icons/md";
 
 const PhotoManagement = () => {
   const posts = useSelector((state) => state.posts.myPosts);
@@ -39,6 +42,21 @@ const PhotoManagement = () => {
       <div>
         <DashboardHeader />
         <ImageAdd />
+      </div>
+
+      <div className="grid grid-cols-1 sm:grid-cols-3 gap-5 mt-5 bg-transparent sm:bg-white p-5 w-[90vw] sm:w-[55vw] sm:h-[95vh] sm:overflow-y-scroll rounded-lg mx-auto sm:mx-0">
+        {posts?.map(({ _id, title, image, author, price }) => (
+          <ImageCard
+            key={_id}
+            id={_id}
+            title={title}
+            img={image}
+            author={author}
+            price={price}
+            icon1={<FaEdit />}
+            icon2={<MdDelete />}
+          />
+        ))}
       </div>
     </div>
   );
