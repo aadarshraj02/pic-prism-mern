e124292f0e6bc17e508438443e0e5020f8d9bec9
aadarshraj02 <aadarshraazz007@gmail.com>
2024-08-14 10:36:01 +0530

    added routes to get favorite

--- server/routes/postroutes.js
+++ server/routes/postroutes.js
@@ -8,6 +8,7 @@ const {
   searchPost,
   addToFavorites,
   removeFromFavorites,
+  getFavorites,
 } = require("../controllers/postController");
 
 router.post("/post/create", verifyToken, createPost);
@@ -16,6 +17,11 @@ router.get("/post/myPosts", verifyToken, getMyPosts);
 router.delete("/post/delete/:id", verifyToken, deletePost);
 router.get("/posts/search", searchPost);
 router.put("/post/addToFavorites/:postId", verifyToken, addToFavorites);
-router.put("/post/removeFromFavorites/:postId", verifyToken, removeFromFavorites);
+router.put(
+  "/post/removeFromFavorites/:postId",
+  verifyToken,
+  removeFromFavorites
+);
+router.get("/posts/favorites", verifyToken, getFavorites);
 
 module.exports = router;
