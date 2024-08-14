219e070981ad988289ba176f233b7be38556c609
aadarshraj02 <aadarshraazz007@gmail.com>
2024-08-14 22:01:50 +0530

    formatted code

--- client/src/ProtectedRoutes.jsx
+++ client/src/ProtectedRoutes.jsx
@@ -1,11 +1,5 @@
-import React from 'react'
-
 const ProtectedRoutes = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  return <div></div>;
+};
 
-export default ProtectedRoutes
+export default ProtectedRoutes;
