a57493139e460ad83f14a3f5a6f7ef076fe980e6
aadarshraj02 <aadarshraazz007@gmail.com>
2024-08-15 10:12:53 +0530

    added h1 and h2 to show purchased and buyed dynamically

--- client/src/components/Analytics.jsx
+++ client/src/components/Analytics.jsx
@@ -1,9 +1,16 @@
 import DashboardHeader from "../components/DashboardHeader";
+import { useLocation } from "react-router-dom";
 
 const Analytics = () => {
+  const { pathname } = useLocation();
+
   return (
     <div>
       <DashboardHeader />
+      <h1 className="text-2xl font-semibold mb-5 ml-8">Analytics</h1>
+      <h2 className="text-2xl font-semibold my-5 ml-8">
+        {pathname == "/seller/profile" ? "Uploaded" : "Purchased"} This Year
+      </h2>
     </div>
   );
 };
