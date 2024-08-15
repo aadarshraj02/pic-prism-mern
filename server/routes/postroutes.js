fa2097572e2f898a0680f87d5ada2d1ccbbb5572
aadarshraj02 <aadarshraazz007@gmail.com>
2024-08-15 09:32:40 +0530

    added routes to get post by daterange

--- server/routes/postroutes.js
+++ server/routes/postroutes.js
@@ -9,19 +9,21 @@ const {
   addToFavorites,
   removeFromFavorites,
   getFavorites,
+  getPostsByRange,
 } = require("../controllers/postController");
 
 router.post("/post/create", verifyToken, createPost);
 router.get("/post/getAll", getAllPosts);
 router.get("/post/myPosts", verifyToken, getMyPosts);
 router.delete("/post/delete/:id", verifyToken, deletePost);
-router.get("/posts/search", searchPost);
+router.get("/post/search", searchPost);
 router.put("/post/addToFavorites/:postId", verifyToken, addToFavorites);
 router.put(
   "/post/removeFromFavorites/:postId",
   verifyToken,
   removeFromFavorites
 );
-router.get("/posts/favorites", verifyToken, getFavorites);
+router.get("/post/favorites", verifyToken, getFavorites);
+router.get("/post/getPostsByDateRange", verifyToken, getPostsByRange);
 
 module.exports = router;
