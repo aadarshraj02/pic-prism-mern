ff1fd3a1bbdf85b0553579cb87ba6a7b62693c96
aadarshraj02 <aadarshraazz007@gmail.com>
2024-08-15 15:09:04 +0530

    displayed image card data dynamically

--- client/src/components/PhotoGallery.jsx
+++ client/src/components/PhotoGallery.jsx
@@ -1,24 +1,52 @@
 import ImageCard from "./ImageCard";
 import { FaShoppingCart } from "react-icons/fa";
 import { IoIosHeart } from "react-icons/io";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+import { setAllPosts } from "../../store/slices/postSlice";
+import { useEffect } from "react";
 
 const PhotoGallery = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const posts = useSelector((state) => state.posts.allPosts);
+  const isAuthenticated = useSelector((state) => state.auth.isAuthenticated);
+
+  const getAllImages = async () => {
+    if (posts.length > 0) return;
+
+    const res = await axios.get(import.meta.env.VITE_API_URL + "/post/getAll");
+    const { data } = await res.data;
+    console.log(data);
+    dispatch(setAllPosts(data));
+  };
+
+  useEffect(() => {
+    getAllImages();
+  }, []);
+
   return (
     <div className="my-20 bg-white flex flex-col justify-center items-center">
       <h3 className="text-3xl font-semibold my-14">Photos</h3>
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-5 mb-20">
-        <ImageCard
-          title="Image Title"
-          author="AuthorName"
-          img="https://plus.unsplash.com/premium_photo-1722586209835-46ae3736ec12?q=80&w=1535&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-          price={0.99}
-          icon1={
-            <FaShoppingCart className="text-2xl text-black cursor-pointer hover:scale-110 transition-all ease-linear duration-300" />
-          }
-          icon2={
-            <IoIosHeart className="text-2xl text-red-500 cursor-pointer hover:scale-110 transition-all ease-linear duration-300" />
-          }
-        />
+        {posts?.map(({ _id, title, image, price, author }) => (
+          <ImageCard
+            key={_id}
+            id={_id}
+            title={title}
+            author={author}
+            img={image}
+            price={price}
+            icon1={
+              <FaShoppingCart className="text-2xl text-black cursor-pointer hover:scale-110 transition-all ease-linear duration-300" />
+            }
+            icon2={
+              <IoIosHeart className="text-2xl text-red-500 cursor-pointer hover:scale-110 transition-all ease-linear duration-300" />
+            }
+          />
+        ))}
       </div>
     </div>
   );
