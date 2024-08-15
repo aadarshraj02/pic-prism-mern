8ea305f4a3f1c7abcac4bbca1d1397c8da73ed48
aadarshraj02 <aadarshraazz007@gmail.com>
2024-08-15 08:53:41 +0530

    added routes for switchProfile

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,8 +1,15 @@
-const { login, signup, refresh } = require("../controllers/authController");
+const { verifyToken } = require("../middlewares/verifyToken");
+const {
+  login,
+  signup,
+  refresh,
+  switchProfile,
+} = require("../controllers/authController");
 
 const router = require("express").Router();
 router.post("/login", login);
 router.post("/signup", signup);
 router.get("/refresh", refresh);
+router.get("/switch", verifyToken, switchProfile);
 
 module.exports = router;
