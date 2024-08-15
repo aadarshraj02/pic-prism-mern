11936f0825dc22295e0a3e0a9578349147326eca
aadarshraj02 <aadarshraazz007@gmail.com>
2024-08-15 11:40:37 +0530

    MOVED TO EXPENSEcARD

--- client/src/components/Analytics.jsx
+++ client/src/components/Analytics.jsx
@@ -1,13 +1,5 @@
 import DashboardHeader from "../components/DashboardHeader";
 import { useLocation } from "react-router-dom";
-import {
-  LineChart,
-  Line,
-  ResponsiveContainer,
-  XAxis,
-  YAxis,
-  Tooltip,
-} from "recharts";
 
 const Analytics = () => {
   const { pathname } = useLocation();
@@ -19,28 +11,7 @@ const Analytics = () => {
       <h2 className="text-2xl font-semibold my-5 ml-8">
         {pathname == "/seller/profile" ? "Uploaded" : "Purchased"} This Year
       </h2>
-      <div className="w-[83vw] sm:w-[80vw] ml-8 p-2 bg-white rounded-2xl shadow-md flex flex-col justify-between items-center gap-5">
-        <ResponsiveContainer width="100%" height={150}>
-          <LineChart
-            margin={{
-              top: 20,
-              bottom: -50,
-              left: -61,
-            }}
-            data={data}
-          >
-            <XAxis dataKey="title" hide />
-            <YAxis />
-            <Tooltip />
-            <Line
-              type="monotone"
-              dataKey="amt"
-              stroke="#8884d8"
-              strokeWidth={2}
-            />
-          </LineChart>
-        </ResponsiveContainer>
-      </div>
+
     </div>
   );
 };
--- client/src/components/ExpenseCard.jsx
+++ client/src/components/ExpenseCard.jsx
@@ -1,5 +1,39 @@
+import {
+  LineChart,
+  Line,
+  ResponsiveContainer,
+  XAxis,
+  YAxis,
+  Tooltip,
+} from "recharts";
+
 const ExpenseCard = () => {
-  return <div></div>;
+  return (
+    <div>
+      <div className="w-[83vw] sm:w-[80vw] ml-8 p-2 bg-white rounded-2xl shadow-md flex flex-col justify-between items-center gap-5">
+        <ResponsiveContainer width="100%" height={150}>
+          <LineChart
+            margin={{
+              top: 20,
+              bottom: -50,
+              left: -61,
+            }}
+            data={data}
+          >
+            <XAxis dataKey="title" hide />
+            <YAxis />
+            <Tooltip />
+            <Line
+              type="monotone"
+              dataKey="amt"
+              stroke="#8884d8"
+              strokeWidth={2}
+            />
+          </LineChart>
+        </ResponsiveContainer>
+      </div>
+    </div>
+  );
 };
 
 export default ExpenseCard;
