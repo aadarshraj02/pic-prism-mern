8773c127cbb8bdb9840ff29c3a5c2c420b2fa910
aadarshraj02 <aadarshraazz007@gmail.com>
2024-08-16 18:21:15 +0530

    added function to get order

--- client/src/components/Orders.jsx
+++ client/src/components/Orders.jsx
@@ -1,9 +1,23 @@
 import axios from "axios";
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import { setOrders } from "../../store/slices/orderSlice";
+import DashboardHeader from "./DashboardHeader";
 
 const Orders = () => {
-  const getOrders = async () => {};
+  const dispatch = useDispatch;
+  const orders = useSelector((state) => state.order.order);
+  const role = useSelector((state) => state.auth.role);
+
+  const getOrders = async () => {
+    const res = await axios.get(import.meta.env.VITE_API_URL + "/orders/get", {
+      headers: {
+        Authorization: "Bearer " + localStorage.getItem("accessToken"),
+      },
+    });
+    const { data } = await res.data;
+    dispatch(setOrders(data));
+  };
 
   useEffect(() => {
     getOrders();
