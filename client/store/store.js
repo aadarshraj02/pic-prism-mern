72f99d01ff0bf87fa50ff0d06b5b73943a50426b
aadarshraj02 <aadarshraazz007@gmail.com>
2024-08-16 18:13:34 +0530

    added reducers of order

--- client/store/store.js
+++ client/store/store.js
@@ -2,11 +2,13 @@ import { configureStore } from "@reduxjs/toolkit";
 import authSlice from "./slices/authSlice";
 import navSlice from "./slices/navSlice";
 import postsSlice from "./slices/postSlice";
+import orderSlice from "./slices/orderSlice";
 
 export const store = configureStore({
   reducer: {
     auth: authSlice,
     nav: navSlice,
     posts: postsSlice,
+    order: orderSlice,
   },
 });
