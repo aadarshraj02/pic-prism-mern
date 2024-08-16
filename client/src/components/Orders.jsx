c89d3211d09d468a8ebaa72a56728892f121154e
aadarshraj02 <aadarshraazz007@gmail.com>
2024-08-16 19:03:51 +0530

    added table and displayed order details

--- client/src/components/Orders.jsx
+++ client/src/components/Orders.jsx
@@ -1,11 +1,11 @@
 import axios from "axios";
-import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { setOrders } from "../../store/slices/orderSlice";
 import DashboardHeader from "./DashboardHeader";
+import { useEffect } from "react";
 
 const Orders = () => {
-  const dispatch = useDispatch(); // Hook used inside the functional component
+  const dispatch = useDispatch();
   const orders = useSelector((state) => state.order.orders);
   const role = useSelector((state) => state.auth.role);
 
@@ -19,9 +19,8 @@ const Orders = () => {
           },
         }
       );
-      const { data } = res;
+      const { data } = await res.data;
       dispatch(setOrders(data));
-      console.log(data);
     } catch (error) {
       console.error("Error fetching orders:", error);
     }
@@ -31,11 +30,49 @@ const Orders = () => {
     getOrders();
   }, []);
 
+  const convertDate = (date) => date.split("T")[0];
+
   return (
     <div>
       <DashboardHeader />
-      <h1 className="text-2xl font-semibold mb-5 ml-8 ">Orders</h1>
-      <div className="overflow-x-auto sm:ml-8 "></div>
+      <h1 className="text-2xl font-semibold mb-5 ml-8">Orders</h1>
+      <div className="overflow-x-auto sm:ml-8">
+        <table className="w-full sm:w-[80vw] bg-white rounded-lg shadow-md">
+          <thead>
+            <tr className="w-full bg-gray-200 text-gray-600 uppercase text-sm leading-normal">
+              <th className="py-3 px-6 text-left">Id</th>
+              <th className="py-3 px-6 text-left">Item</th>
+              <th className="py-3 px-6 text-left">
+                {role === "seller" ? "Purchaser" : "Author"} Name
+              </th>
+              <th className="py-3 px-6 text-left">Date</th>
+              <th className="py-3 px-6 text-right">Price</th>
+            </tr>
+          </thead>
+          <tbody className="text-gray-600 text-sm font-light">
+            {orders?.map((order) => (
+              <tr
+                key={order.razorpayOrderId}
+                className="border-b border-gray-200 hover:bg-gray-100"
+              >
+                <td className="py-3 px-6 text-left">{order.razorpayOrderId}</td>
+                <td className="py-3 px-6 text-left">{order.title}</td>
+                <td className="py-3 px-6 text-left">
+                  {role === "buyer"
+                    ? order.author?.charAt(0).toUpperCase() +
+                        order.author?.slice(1) || "Unknown Author"
+                    : order.purchaser?.charAt(0).toUpperCase() +
+                        order.purchaser?.slice(1) || "Unknown Purchaser"}
+                </td>
+                <td className="py-3 px-6 text-left">
+                  {convertDate(order.createdAt)}
+                </td>
+                <td className="py-3 px-6 text-right">${order.price}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
