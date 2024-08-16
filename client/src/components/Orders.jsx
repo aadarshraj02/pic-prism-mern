bbbe1d9af1e1c58f92a8b3448842370ada673e1a
aadarshraj02 <aadarshraazz007@gmail.com>
2024-08-16 18:33:48 +0530

    fixed issues with order data

--- client/src/components/Orders.jsx
+++ client/src/components/Orders.jsx
@@ -2,28 +2,35 @@ import axios from "axios";
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { setOrders } from "../../store/slices/orderSlice";
-import DashboardHeader from "./DashboardHeader";
 
 const Orders = () => {
-  const dispatch = useDispatch;
-  const orders = useSelector((state) => state.order.order);
+  const dispatch = useDispatch(); // Hook used inside the functional component
+  const orders = useSelector((state) => state.order.orders);
   const role = useSelector((state) => state.auth.role);
 
   const getOrders = async () => {
-    const res = await axios.get(import.meta.env.VITE_API_URL + "/orders/get", {
-      headers: {
-        Authorization: "Bearer " + localStorage.getItem("accessToken"),
-      },
-    });
-    const { data } = await res.data;
-    dispatch(setOrders(data));
+    try {
+      const res = await axios.get(
+        import.meta.env.VITE_API_URL + "/orders/get",
+        {
+          headers: {
+            Authorization: "Bearer " + localStorage.getItem("accessToken"),
+          },
+        }
+      );
+      const { data } = res;
+      dispatch(setOrders(data));
+      console.log(data);
+    } catch (error) {
+      console.error("Error fetching orders:", error);
+    }
   };
 
   useEffect(() => {
     getOrders();
   }, []);
 
-  return <div></div>;
+  return <div>{/* Render your orders or other content here */}</div>;
 };
 
 export default Orders;
