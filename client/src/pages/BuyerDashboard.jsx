76cfa8d5794077b32dabcd6d1fda268a60b9b075
aadarshraj02 <aadarshraazz007@gmail.com>
2024-08-17 19:11:33 +0530

    added buyerdashboard

--- client/src/pages/BuyerDashboard.jsx
+++ client/src/pages/BuyerDashboard.jsx
@@ -1,5 +1,30 @@
+import DashboardSidebar from "../components/DashboardSidebar";
+import PhotoManagement from "../components/seller/PhotoManagement";
+import Analytics from "../components/Analytics";
+import Orders from "../components/Orders";
+import { useSelector } from "react-redux";
+
 const BuyerDashboard = () => {
-  return <div></div>;
+  const tab = useSelector((state) => state.nav.tab);
+  return (
+    <div className="flex flex-col sm:flex-row">
+      <DashboardSidebar />
+      <div>
+        {(() => {
+          switch (tab) {
+            case "photos-management":
+              return <PhotoManagement />;
+            case "analytics":
+              return <Analytics />;
+            case "orders":
+              return <Orders />;
+            default:
+              return <PhotoManagement />;
+          }
+        })()}
+      </div>
+    </div>
+  );
 };
 
 export default BuyerDashboard;
