21d8efb0dcb72231601a86d499d23c495ae6cd71
aadarshraj02 <aadarshraazz007@gmail.com>
2024-08-17 20:53:46 +0530

    formatted code

--- client/src/components/buyer/PhotosPurchased.jsx
+++ client/src/components/buyer/PhotosPurchased.jsx
@@ -1,16 +1,17 @@
+import { useEffect } from "react";
 import toast from "react-hot-toast";
 import { logout } from "../../../store/slices/authSlice";
 import { useDispatch, useSelector } from "react-redux";
-import { useEffect } from "react";
 import axios from "axios";
 import DashboardHeader from "../DashboardHeader";
 import ImageCard from "../ImageCard";
-import { RxDownload } from "react-icons/rx";
 import { setMyPosts } from "../../../store/slices/postSlice";
+import { IoArrowDownCircle } from "react-icons/io5";
 
 const PhotosPurchased = () => {
   const dispatch = useDispatch();
-  const posts = useSelector((state) => state / posts.myPosts);
+
+  const posts = useSelector((state) => state.posts.myPosts);
 
   const getMyPosts = async () => {
     try {
@@ -19,14 +20,14 @@ const PhotosPurchased = () => {
         import.meta.env.VITE_API_URL + "/post/myPosts",
         {
           headers: {
-            Authorization: "Bearer " + localStorage.getItem("AccessToken"),
+            Authorization: "Bearer " + localStorage.getItem("accessToken"),
           },
         }
       );
       const { data } = await res.data;
       dispatch(setMyPosts(data));
     } catch (error) {
-      toast.error("Failed to fetch Photos", error);
+      toast.error("Failed to fetch photos", error);
       dispatch(logout());
     }
   };
@@ -38,7 +39,7 @@ const PhotosPurchased = () => {
   const downloadImage = async (image, title) => {
     try {
       const response = await fetch(image);
-      if (!response.ok) throw new Error("Failed to download Image");
+      if (!response.ok) throw new Error("Failed to download image");
       const blob = await response.blob();
       const url = URL.createObjectURL(blob);
       const a = document.createElement("a");
@@ -49,25 +50,26 @@ const PhotosPurchased = () => {
       document.body.removeChild(a);
       URL.revokeObjectURL(url);
     } catch (error) {
-      toast.error("Failed to download Image", error);
+      console.log("Error downloading image ", error);
     }
   };
 
   return (
     <div>
       <DashboardHeader />
-      <div className="mx-8 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
+      <div className="mx-8 grid md:grid-cos-3 lg:grid-cols-4 gap-4">
         {posts?.map(({ _id, title, postUrl, author, price }) => (
           <ImageCard
             key={_id}
             title={title}
-            author={author}
             price={price}
+            author={author}
+            img={postUrl}
             icon2={
-              <RxDownload
-                onClick={() => downloadImage(postUrl, title)}
+              <IoArrowDownCircle
                 title="Download Now"
-                className="text-2xl text-red-500 cursor-pointer hover:scale-110 transition-all duration-300 ease-linear "
+                className="text-2xl  text-red-500 cursor-pointer hover:scale-110 transition-all ease-linear duration-300"
+                onClick={() => downloadImage(postUrl, title)}
               />
             }
           />
