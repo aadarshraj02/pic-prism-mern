acec65911ea640f5a2743cda6beaecf9d41dcc1f
aadarshraj02 <aadarshraazz007@gmail.com>
2024-08-17 22:04:57 +0530

    added form input and label for name email and message and added a send button

--- client/src/pages/Contact.jsx
+++ client/src/pages/Contact.jsx
@@ -1,7 +1,65 @@
 const Contact = () => {
   return (
-    <div className="h-screen flex flex-col items-center justify-center">
-      <h1>Contact Us</h1>
+    <div className="mt-20 sm:mt-10 h-[90vh] flex items-center justify-center w-full ">
+      <div className="bg-white shadow-md rounded-3xl px-5 py-6 w-full sm:w-[27vw]">
+        <h1 className="text-2xl font-bold text-center mb-4">Contact Us</h1>
+        <p className="text-sm text-center mb-4 text-gray-500">
+          Ask us anything and get reply back within 24h
+        </p>
+        <form>
+          <div className="mb-4">
+            <label
+              htmlFor="fullName"
+              className="block text-sm font-medium text-gray-700 mb-2"
+            >
+              Full Name
+            </label>
+            <input
+              type="text"
+              name="fullName"
+              id="fullName"
+              placeholder="Enter Your Full Name"
+              className="shadow-md rounded-md w-full px-3 py-2 border-gray-300 focus:outline-none focus:ring-black focus:border-black"
+            />
+          </div>
+          <div className="mb-4">
+            <label
+              htmlFor="Email"
+              className="block text-sm font-medium text-gray-700 mb-2"
+            >
+              Your Email
+            </label>
+            <input
+              type="email"
+              name="email"
+              id="email"
+              placeholder="Your@maiul.com"
+              className="shadow-md rounded-md w-full px-3 py-2 border-gray-300 focus:outline-none focus:ring-black focus:border-black"
+            />
+          </div>
+          <div className="mb-4">
+            <label
+              htmlFor="message"
+              className="block text-sm font-medium text-gray-700 mb-2"
+            >
+              Your Message
+            </label>
+            <textarea
+              name="message"
+              id="message"
+              rows="3"
+              cols="50"
+              className="shadow-md rounded-md w-full px-3 py-2 border-gray-300 focus:outline-none focus:ring-black focus:border-black"
+            />
+          </div>
+          <button
+            type="submit"
+            className="w-full py-2 px-4 rounded-md shadow-md text-sm font-medium text-white bg-black"
+          >
+            Send
+          </button>
+        </form>
+      </div>
     </div>
   );
 };
