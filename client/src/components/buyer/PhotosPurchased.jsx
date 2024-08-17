ece759533adec8588c1dd0c3440eba4fd441a7c9
aadarshraj02 <aadarshraazz007@gmail.com>
2024-08-17 19:20:58 +0530

    added function to get my posts

--- client/src/components/buyer/PhotosPurchased.jsx
+++ client/src/components/buyer/PhotosPurchased.jsx
@@ -1,4 +1,35 @@
+import toast from "react-hot-toast";
+import { logout } from "../../../store/slices/authSlice";
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
+import axios from "axios";
+
 const PhotosPurchased = () => {
+  const dispatch = useDispatch();
+  const posts = useSelector((state) => state / posts.myPosts);
+
+  const getMyPosts = async () => {
+    try {
+      if (posts.length > 0) return;
+      const res = await axios.get(
+        import.meta.env.VITE_API_URL + "/post/myPosts",
+        {
+          headers: {
+            Authorization: "Bearer " + localStorage.getItem("AccessToken"),
+          },
+        }
+      );
+      const { data } = await res.data;
+    } catch (error) {
+      toast.error("Failed to fetch Photos", error);
+      dispatch(logout());
+    }
+  };
+
+  useEffect(() => {
+    getMyPosts();
+  }, []);
+
   return <div></div>;
 };
 
