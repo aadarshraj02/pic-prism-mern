da25890a77b870db38f39b0aecdb36c8558fd712
aadarshraj02 <aadarshraazz007@gmail.com>
2024-08-17 19:02:44 +0530

    added switchcase to switch components

--- client/src/pages/SellerDashboard.jsx
+++ client/src/pages/SellerDashboard.jsx
@@ -1,15 +1,28 @@
+import { useSelector } from "react-redux";
 import Analytics from "../components/Analytics";
-import DashboardSidebar from "../components/DashboardSidebar";
+// import DashboardSidebar from "../components/DashboardSidebar";
 import Orders from "../components/Orders";
-// import PhotoManagement from "../components/seller/PhotoManagement";
+import PhotoManagement from "../components/seller/PhotoManagement";
 
 const SellerDashboard = () => {
+  const tab = useSelector((state) => state.nav.tab);
+
   return (
     <div className="flex flex-col sm:flex-row">
-      <DashboardSidebar />
-      {/* <PhotoManagement /> */}
-      <Analytics />
-      {/* <Orders /> */}
+      <div>
+        {(() => {
+          switch (tab) {
+            case "photos-management":
+              return <PhotoManagement />;
+            case "analytics":
+              return <Analytics />;
+            case "orders":
+              return <Orders />;
+            default:
+              break;
+          }
+        })()}
+      </div>
     </div>
   );
 };
