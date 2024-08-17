92567a3f7729e1d5f4c47aa8c68bdd21bf6e8510
aadarshraj02 <aadarshraazz007@gmail.com>
2024-08-17 23:04:22 +0530

    added image

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -2,6 +2,10 @@ const About = () => {
   return (
     <div className="mt-32 flex flex-col">
       <h1 className="text-center text-2xl font-semibold">About Us</h1>
+      <div>
+        <div><img src="../../public/custom-nature-photo-collage-design-template-2b09520cddb78c6020d53e0671bb2453_screen.jpg" alt="" /></div>
+        <div></div>
+      </div>
     </div>
   );
 };
