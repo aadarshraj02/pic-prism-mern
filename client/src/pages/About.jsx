b04e20c9ca45b4f7ee14d629775df35ebceb0a63
aadarshraj02 <aadarshraazz007@gmail.com>
2024-08-17 23:49:59 +0530

    added image

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -21,6 +21,37 @@ const About = () => {
             how much you wanna sell your clicked pics. You will get all stats
             like how much you earned spend and all details Try and know more...
           </p>
+          <div className="w-full flex flex-col sm:flex-row gap-4">
+            <div className="sm:w-1/2">
+              <p className="mt-2 text-sm">
+                <span className="text-gray-700 text-xl font-semibold">
+                  As a Seller:
+                </span>{" "}
+                <br />
+                After signing up as a Seller on your dashboard you can Upload
+                your assets and then fill the form in which you have to provide
+                title and set your price. You will also get details like order
+                details and analytics graph for this week month and this year.
+              </p>
+              <p className="mt-2 text-sm">
+                <span className="text-gray-700 text-xl font-semibold">
+                  As a Buyer:
+                </span>{" "}
+                <br />
+                After signing up as a Buyer on your dashboard you can purchase
+                any assets and then it will be displayed in your profile. You
+                can also check your analytics like how much you spend this week,
+                month and this Year and you will also get your all order details.
+              </p>
+            </div>
+            <div className="w-1/2">
+              <img
+                className="w-full object-cover hidden md:block"
+                src="/image2.webp"
+                alt=""
+              />
+            </div>
+          </div>
         </div>
       </div>
     </div>
