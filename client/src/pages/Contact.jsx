4a6a236bb6cdafd55e223928acef033cc8d54aef
aadarshraj02 <aadarshraazz007@gmail.com>
2024-08-17 21:08:51 +0530

    Display Form

--- client/src/pages/Contact.jsx
+++ client/src/pages/Contact.jsx
@@ -1,5 +1,9 @@
 const Contact = () => {
-  return <div></div>;
+  return (
+    <div className="h-screen flex flex-col items-center justify-center">
+      <h1>Contact Us</h1>
+    </div>
+  );
 };
 
 export default Contact;
