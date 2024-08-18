b10ef33a175efb6078d3c696af77d974658b4df8
aadarshraj02 <aadarshraazz007@gmail.com>
2024-08-18 09:47:34 +0530

    validated login form

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -13,6 +13,25 @@ const Login = () => {
 
   const handleLogin = async (e) => {
     e.preventDefault();
+
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (email === "") {
+      toast.error("Email is required");
+      return;
+    } else if (!emailRegex.test(email)) {
+      toast.error("Please enter a valid email address");
+      return;
+    }
+
+    const passwordRegex = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/;
+    if (password === "") {
+      toast.error("Password is required");
+      return;
+    } else if (!passwordRegex.test(password)) {
+      toast.error("Invalid Password");
+      return;
+    }
+
     try {
       const res = await axios.post(import.meta.env.VITE_API_URL + "/login", {
         email,
@@ -47,6 +66,7 @@ const Login = () => {
               id="email"
               placeholder="your@email.com"
               value={email}
+              required
               onChange={(e) => setEmail(e.target.value)}
               className="shadow-md rounded-md w-full px-3 py-2 border-gray-300 focus:outline-none focus:ring-black focus:border-black"
             />
@@ -64,6 +84,7 @@ const Login = () => {
               id="password"
               placeholder="Enter any Password"
               value={password}
+              required
               onChange={(e) => setPassword(e.target.value)}
               className="shadow-md rounded-md w-full px-3 py-2 border-gray-300 focus:outline-none focus:ring-black focus:border-black"
             />
